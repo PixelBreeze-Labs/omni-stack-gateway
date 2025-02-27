4b9a4012fbf35c1ae1442d9f4c5cfdb61b034345
Griseld <development@venueboost.io>
2025-02-27 21:41:35 +0200

    increase nestjs upload file

---
+++ src/interfaces/staff-user.interface.ts
@@ -0,0 +1,20 @@
+// src/interfaces/staff-user.interface.ts
+
+import { User } from '../schemas/user.schema';
+import { Business } from '../schemas/business.schema';
+
+export interface StaffUserParams {
+    page?: number;
+    limit?: number;
+    search?: string;
+    sort?: string;
+}
+
+export interface StaffUserResponse {
+    items: Array<{
+        user: User;
+        businesses: Business[];
+    }>;
+    total: number;
+    pages: number;
+}
