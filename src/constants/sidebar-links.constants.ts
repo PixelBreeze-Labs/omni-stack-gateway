026181dab26536f86f4c7651c096da1206997c5e
Griseld <development@venueboost.io>
2025-06-01 00:12:11 +0300

    fix

--- src/constants/sidebar-links.constants.ts
+++ src/constants/sidebar-links.constants.ts
@@ -119,25 +119,34 @@ export const teamLeaderLinks: SideLink[] = [
         title: 'Projects',
         href: '/team-leader/projects',
         icon: null,
+        sub: [
+            {
+                title: 'Manage',
+                href: '/team-leader/projects',
+                icon: null,
+            }
+        ]
     },
     {
         title: 'Tasks',
         href: '/team-leader/tasks',
         icon: null,
+        sub: [
+            {
+                title: 'Manage',
+                href: '/team-leader/tasks',
+                icon: null,
+            }
+        ]
     },
     {
         title: 'Time Tracking',
         href: '/team-leader/time-tracking',
         icon: null,
-    },
-    {
-        title: 'Team',
-        href: '/team-leader',
-        icon: null,
         sub: [
             {
-                title: 'Employees',
-                href: '/team-leader/employees',
+                title: 'Manage',
+                href: '/team-leader/time-tracking',
                 icon: null,
             }
         ]
